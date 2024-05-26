e74524e00634f0a4a8db1aa65d3bfe2ab0b30fd5
Thiago <thiago020698@gmail.com>
2024-05-26 19:20:33 -0300

    Criado todas as paginas e redirecionamentos

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.base, name="base")
+    path("", views.base, name="base"),
+    path("home", views.home, name="home"),
+    path("relato", views.relato, name="relato"),
+    path("dash", views.dash, name="dash"),
+    path("gestao", views.gestao, name="gestao"),
+    path("config", views.config, name="config"),
+
+    path("n_relato", views.n_relato, name="n_relato"),
+    path("estra", views.estra, name="estra"),
+    path("l_relatos", views.l_relatos, name="l_relatos"),
+    path("l_estra", views.l_estra, name="l_estra"),
+
+    path("user", views.user, name="user"),
+    path("unidade", views.unidade, name="unidade"),
+    path("turno", views.turno, name="turno"),
+    path("setor", views.setor, name="setor"),
+    path("cargo", views.cargo, name="cargo"),
+
+    path("d_rel", views.d_rel, name="d_rel"),
+    path("d_estra", views.d_estra, name="d_estra"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,38 @@ from django.shortcuts import render
 
 def base(request):
     return render(request, "base.html")
+def home(request):
+    return render(request, "home/home.html")
+def relato(request):
+    return render(request, "relatos/relato.html")
+def dash(request):
+    return render(request, "dashboard/dash.html")
+def gestao(request):
+    return render(request, "gestao/gestao.html")
+def config(request):
+    return render(request, "config/config.html")
+
+def n_relato(request):
+    return render(request, "relatos/n_relato.html")
+def estra(request):
+    return render(request, "relatos/estra.html")
+def l_relatos(request):
+    return render(request, "relatos/l_relato.html")
+def l_estra(request):
+    return render(request, "relatos/l_estra.html")
+
+def user(request):
+    return render(request, "gestao/user.html")
+def unidade(request):
+    return render(request, "gestao/unidade.html")
+def turno(request):
+    return render(request, "gestao/turno.html")
+def setor(request):
+    return render(request, "gestao/setor.html")
+def cargo(request):
+    return render(request, "gestao/cargo.html")
+
+def d_rel(request):
+    return render(request, "dashboard/d_rel.html")
+def d_estra(request):
+    return render(request, "dashboard/d_rel.html")
